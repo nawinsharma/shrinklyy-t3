9807115644235931d2b742d5326e84e2946fed75
nawinsharma <nawinsharma60@gmail.com>
2025-06-21 15:37:57 +0530

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { Link, Zap } from 'lucide-react'
+import { Zap } from 'lucide-react'
 import LandingNavbar from '@/components/LandingNavbar'
 import AnimatedGradient from '@/components/AnimatedGradient'
 import ElegantShape from '@/components/ElegantShape'
